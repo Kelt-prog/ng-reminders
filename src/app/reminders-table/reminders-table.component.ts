086d5330dca544d01a35ef8b82fb2592cd5f4eda
Dmitry Mordvintsev <d.mordvincev@systtech.ru>
2020-08-25 11:17:35 +0200

    table reminders created

---
+++ src/app/reminders-table/reminders-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'reminders-table',
+  templateUrl: './reminders-table.component.html',
+  styleUrls: ['./reminders-table.component.scss']
+})
+export class RemindersTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
